84d351bba05231f04c5a175e59769522afd2ad33
Daniil Detter <vudi600@gmail.com>
2023-05-29 20:01:55 +0700

    Update

--- extension/js/TableCleaner.js
+++ extension/js/TableCleaner.js
@@ -1,42 +1,29 @@
-// Создаем функцию для удаления строк таблицы
 function removeTableRows() {
-  // Получаем все строки таблицы с классом "ng-scope do-not-need-to-study"
   var rows = document.querySelectorAll('.ng-scope.do-not-need-to-study');
-  
-  // Перебираем строки и удаляем их
-  rows.forEach(function(row) {
+  rows.forEach(function (row) {
     row.parentNode.removeChild(row);
   });
 }
 
-// Создаем функцию для отслеживания изменений в DOM
 function observeDOM() {
-  // Создаем новый экземпляр MutationObserver
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      // Проверяем, есть ли на странице таблица с классом "ng-scope do-not-need-to-study"
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
       if (document.querySelector('.ng-scope.do-not-need-to-study')) {
-        // Если таблица появилась, удаляем строки
         removeTableRows();
-        
-        // Отключаем наблюдение, так как больше не нужно
         observer.disconnect();
       }
     });
   });
-  
-  // Настраиваем наблюдение на изменения внутри <body> элемента
+
   observer.observe(document.body, { childList: true, subtree: true });
 }
 
-// Слушаем событие загрузки страницы
-document.addEventListener('DOMContentLoaded', function() {
-  // Проверяем, есть ли на странице таблица с классом "ng-scope do-not-need-to-study"
-  if (document.querySelector('.ng-scope.do-not-need-to-study')) {
-    // Если таблица уже присутствует, удаляем строки
-    removeTableRows();
-  } else {
-    // Если таблица еще не появилась, запускаем отслеживание изменений в DOM
-    observeDOM();
-  }
-});
+if (window.location.hostname === 'ocenka.tusur.ru') {
+  document.addEventListener('DOMContentLoaded', function () {
+    if (document.querySelector('.ng-scope.do-not-need-to-study')) {
+      removeTableRows();
+    } else {
+      observeDOM();
+    }
+  });
+}
--- extension/js/filter.js
+++ extension/js/filter.js
@@ -1,10 +1,10 @@
-function editCourse(event){
+function editCourse(event) {
     let course_1 = [6701, 6533, 11583, 10157, 11801, 11301, 9075, 10972, 10972, 11150, 9659, 11206, 11834, 10263, 10042, 9399, 6359, 12565];
     let course_2 = [6701, 4699, 4739, 12991, 14178, 10477, 14887, 15047, 14737, 14837, 13444, 13494, 13311, 13264, 13138, 13389, 9162, 6672, 6359];
     let now = [4739, 14887, 15047, 14737, 14837, 13311, 6672, 12565, 6701]
 
     var courses_list = document.getElementsByClassName("courses frontpage-course-list-enrolled")[0].childNodes;
-    
+
     switch (event.target.value) {
         case 'Курс 1':
             Array.from(courses_list).forEach(element => {
@@ -13,7 +13,7 @@ function editCourse(event){
                     element.hidden = true;
                 }
             });
-            break;  
+            break;
         case 'Курс 2':
             Array.from(courses_list).forEach(element => {
                 element.hidden = false;
@@ -46,24 +46,25 @@ function addEvent() {
 }
 
 function onLoad() {
-    var div = document.createElement("div"); 
+    var div = document.createElement("div");
     var filter = document.getElementsByClassName("course-search-container alone")[0];
-    div.innerHTML='<div style="margin-top: 10px">'+
-                    '<form class="course_filter_forms">'+
-                        '<input type="submit" value="Курс 1" data-toggle="button" class="btn_course">'+
-                    '</form>'+
-                    '<form class="course_filter_forms">'+
-                        '<input type="submit" value="Курс 2" data-toggle="button" class="btn_course">'+
-                    '</form>'+
-                    '<form class="course_filter_forms">'+
-                        '<input type="submit" value="Полезное сейчас" data-toggle="button" class="btn_course">'+
-                    '</form>'+
-                    '<form class="course_filter_forms">'+
-                        '<input type="submit" value="Сбросить" data-toggle="button" class="btn_course">'+
-                    '</form>'+
-                '</div>'
+    div.innerHTML = '<div style="margin-top: 10px">' +
+        '<form class="course_filter_forms">' +
+        '<input type="submit" value="Курс 1" data-toggle="button" class="btn_course">' +
+        '</form>' +
+        '<form class="course_filter_forms">' +
+        '<input type="submit" value="Курс 2" data-toggle="button" class="btn_course">' +
+        '</form>' +
+        '<form class="course_filter_forms">' +
+        '<input type="submit" value="Полезное сейчас" data-toggle="button" class="btn_course">' +
+        '</form>' +
+        '<form class="course_filter_forms">' +
+        '<input type="submit" value="Сбросить" data-toggle="button" class="btn_course">' +
+        '</form>' +
+        '</div>'
     filter.appendChild(div);
     addEvent();
 }
-
-document.addEventListener("DOMContentLoaded", onLoad, false);
+if (window.location.href == "https://sdo.tusur.ru/") {
+    document.addEventListener("DOMContentLoaded", onLoad, false);
+}
