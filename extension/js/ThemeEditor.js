cf7b3f64d6d0e4c30a613f671c2c750c281863f9
Daniil Detter <vudi600@gmail.com>
2023-05-31 01:45:32 +0700

    Add styles

--- extension/js/ThemeEditor.js
+++
@@ -1,9 +0,0 @@
-function onLoad() {
-    var theme_href = chrome.runtime.getURL("styles/theme.css");
-    var link = document.createElement("link");
-    link.href = theme_href;
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    document.getElementsByTagName("head")[0].appendChild(link);
-}
-document.addEventListener("DOMContentLoaded", onLoad, false);
